3a2a75dc28083ae0269c633a2b11df680b4321ae
유하은 <78515302+YHE0663@users.noreply.github.com>
2026-02-13 02:59:49 +0900

    chore: greedy 알고리즘 풀이(5)

--- templates/javascript/greedy/05.js
+++ templates/javascript/greedy/05.js
@@ -13,14 +13,20 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let [a, b] = input[0].split(" ").map(Number); // A와 B 입력
 let flag = false;
-let reult = 1;
+let result = 1;
 
 while (a <= b) {
   if (a == b) {
     flag = true;
     break;
   }
-  if (b % 2 === 0)
-    b = parseInt(b / 2); // 2로 나누어 떨어지는 경우
-  else if (b % 2 === 1) resulr = true;
+  if (b % 2 === 0) b = parseInt(b / 2);
+  // 2로 나누어 떨어지는 경우
+  else if (b % 10 == 1)
+    b = parseInt(b / 10); // 그렇지 않고, 일의 자릿수가 1인 경으
+  else break; // 위 경우가 모두 해당되지 않은 경우
+  result++;
 }
+
+if (flag) console.log(result);
+else console.log(-1);
