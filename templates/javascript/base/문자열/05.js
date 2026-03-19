fd672cfacf0bc0e65d3ed2acbe004d41a0bf809f
유하은 <78515302+YHE0663@users.noreply.github.com>
2026-03-19 22:53:05 +0900

    chore: 알고리즘 해석  수정

--- templates/javascript/base/문자열/05.js
+++ templates/javascript/base/문자열/05.js
@@ -1,3 +1,7 @@
+/**
+ * 단순히 전체 문자열에 포함된 단어의 개수를 계산한다.
+ * 결과적으로 공백(" ")으로 구분된 단어의 개수를 세면 된다.
+ */
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
--- templates/javascript/sorting/04.js
+++ templates/javascript/sorting/04.js
@@ -3,7 +3,6 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 // case 1
 let resultIndex = input[0].split(" ").map(Number)[1];
-let [n, k] = input[0].split(" ").map(Number);
 let arr = input[1].split(" ").map(Number);
 
 arr.sort((a, b) => a - b);
